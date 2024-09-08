166c0c4e61b11b9a9834a5ff13e716fc683365c3
BestTTTA <bst12546@gmail.com>
2024-09-08 11:12:14 +0700

    Add optimize page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import GetProjects from '../services/getAllProduct';
 import { Projects } from '../services/type';
 import Link from 'next/link';
+import { GrOptimize } from "react-icons/gr";
+import Image from 'next/image';
 const Admin = () => {
     const [projects, setProjects] = useState<Projects[] | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
@@ -40,11 +42,17 @@ const Admin = () => {
                             <Link href={{
                                 pathname: '/admin_projects',
                                 query: { name_project: project.name_project_head },
-                            }} className='bg-white w-full rounded-md p-2 mb-2 hover:scale-105 drop-shadow-md'>
+                            }} className='bg-white w-full rounded-md p-2 mb-2 hover:scale-102 drop-shadow-md'>
                                 <p className='text-orange-500'>{project.name_project_head}</p>
                             </Link>
                         </div>
                     ))}
+                </div>
+                <div className='absolute bottom-2 right-2'>
+                    <Link href="https://battrack-optimization-web.vercel.app" className='flex flex-col items-center hover:scale-105'>
+                        <GrOptimize size={50} color='orange' />
+                        <p className='text-orange-500'>Optimize</p>
+                    </Link>
                 </div>
             </main>
         );
